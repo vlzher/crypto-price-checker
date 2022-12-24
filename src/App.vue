0092f9d61d30e507328d072de1065de205bdb63a
Władysław <89138444+vlzher@users.noreply.github.com>
2022-12-24 17:28:10 +0100

    fix loading

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
+    <div
+      v-if="!isMounted"
+      class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center"
+    >
+      <svg
+        class="animate-spin -ml-1 mr-3 h-12 w-12 text-white"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          class="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+        ></circle>
+        <path
+          class="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+    </div>
     <div class="container">
       <section>
         <div class="flex">
@@ -201,6 +226,7 @@ export default {
 
   data() {
     return {
+      isMounted: false,
       inputText: "",
       currentCoin: { name: "", price: 0 },
       coinList: [],
@@ -227,6 +253,12 @@ export default {
       ],
       graph: [],
     };
+  },
+  beforeMount() {
+    this.isMounted = false;
+  },
+  mounted() {
+    this.isMounted = true;
   },
 };
 </script>
